e166d6b63f86c61ab179f27e82ed14a1189b668f
b1zman <example@email.comtm121art@mail.ru>
2021-10-13 23:59:12 +0300

    Fixed

--- hw2_epam_lab/src/main/java/hw2/Main.java
+++ hw2_epam_lab/src/main/java/hw2/Main.java
@@ -1,10 +1,8 @@
 package hw2;
 
-import java.util.SortedMap;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Fibonachi.сalculate(0, 11));
-        System.out.println(Fucktorial.calculate(2,3));
+        System.out.println(Fibonacci.сalculate(2, 11));
+        System.out.println(Factorial.calculate(1, 4));
     }
 }
