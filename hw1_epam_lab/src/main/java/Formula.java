06f94fa2f0e86a3c6a9603825fe0f45413e73113
b1zman <example@email.comtm121art@mail.ru>
2021-10-13 22:08:55 +0300

    Fixed

--- hw1_epam_lab/src/main/java/Formula.java
+++ hw1_epam_lab/src/main/java/Formula.java
@@ -1,13 +1,7 @@
 public class Formula {
-    private int a;
-    private int p;
-    private double m1;
-    private double m2;
-
 
     public static double getResultG(int a, int p, double m1, double m2) {
         return (4 * Math.pow(Math.PI, 2) * Math.pow(a, 3)) / (Math.pow(p, 2) * (m1 + m2));
         //System.out.printf("%.2f",Formula.getResultG());
     }
 }
-
